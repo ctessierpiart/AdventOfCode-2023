94807873e56726b49eee4057c4dcb1a08d6a7acf
ctessierpiart <c.tessierpiart@gmail.com>
2023-12-01 11:07:21 +0100

    Other solution for Day1

--- Day01/Solve.py
+++ Day01/Solve.py
@@ -25,18 +25,32 @@ def decode_art_better(art : str):
             'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     occurences = [art.rfind(num) for num in numbers]
     last_digit_idx = occurences.index(max(occurences))
+
     occurences = [art.find(num) for num in numbers]
     for idx, occ in enumerate(occurences):
         if occ == -1:
             occurences[idx] = 100
     first_digit_idx = occurences.index(min(occurences))
+
     if last_digit_idx >= 9:
         last_digit_idx -= 9
     if first_digit_idx >= 9:
         first_digit_idx -= 9
+        
     value = int(str(first_digit_idx+1) + str(last_digit_idx+1))
-    print(f'{art} ==> {value}')
     return value
+
+    # Shorter but I don't like it
+    
+    # str_numbers = ['oneight', 'nineight', 'fiveight', 'twone', 'eightwo', 'eighthree', 'sevenine', 'threeight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    # numbers = ['18', '98', '58', '21', '82', '83', '79', '38', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    # str_art = art
+    # for i in range(len(str_numbers)):
+    #     str_art = str_art.replace(str_numbers[i], numbers[i])
+    
+    # return decode_art(str_art)
+
+
 
 better_calibration = 0
 for idx, art in enumerate(calibration_values):
