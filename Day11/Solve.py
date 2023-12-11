9c060e4183de3d302fa0ea0feae4f4d823cddc0a
ctessierpiart <c.tessierpiart@gmail.com>
2023-12-11 16:26:01 +0100

    Tidying up

--- Day11/Solve.py
+++ Day11/Solve.py
@@ -5,6 +5,18 @@ class Galaxy:
         self.x = coord[0]
         self.y = coord[1]
 
+def findGaps(axis, dictGlaxy):
+    if axis == 'x':
+        listPos =  [galaxy.x for galaxy in dictGlaxy.values()]
+    else:
+        listPos =  [galaxy.y for galaxy in dictGlaxy.values()]
+    listVoid = []
+    for idx in range(max(listPos)):
+        if idx in listPos:
+            continue
+        listVoid.append(idx)
+    return listVoid 
+
 def timeDilation(listVoidX, listVoidY, galaxy, time):
     Xcount, Ycount = 0, 0
     for X in listVoidX:
@@ -30,20 +42,10 @@ with open('Day11/Input.txt') as file:
                 Galaxies[str(galaxyCount)] = Galaxy((idxChar, idxLine))
                 galaxyCount += 1
 
-listX = [galaxy.x for galaxy in Galaxies.values()]
-listY = [galaxy.y for galaxy in Galaxies.values()]
-listVoidX = []
-listVoidY = []
-for idxX in range(max(listX)):
-    if idxX in listX:
-        continue
-    listVoidX.append(idxX)
-for idxY in range(max(listY)):
-    if idxY in listY:
-        continue
-    listVoidY.append(idxY)
-Galaxies_Part2 = Galaxies
-#Part 1 : 1; Part 2 : 1000000
+listVoidX = findGaps('x', Galaxies)
+listVoidY = findGaps('y', Galaxies)
+
+#Part 1 : 2; Part 2 : 1000000
 dilation = 1000000
 for galaxy in Galaxies.values():
     timeDilation(listVoidX, listVoidY, galaxy,dilation -1)
@@ -51,9 +53,9 @@ for galaxy in Galaxies.values():
 Pair_dist = {}
 for galaxy_key, galaxy in Galaxies.items():
     for pair_key, pair in Galaxies.items():
-        if galaxy_key + '-' + pair_key in Pair_dist.keys():
+        if galaxy_key + '-' + pair_key in Pair_dist.keys() or pair_key + '-' + galaxy_key in Pair_dist.keys():
             continue
         Pair_dist[galaxy_key + '-' + pair_key] = measureDist(galaxy, pair)
 
-sumDist = sum([dist for dist in Pair_dist.values()])//2
+sumDist = sum([dist for dist in Pair_dist.values()])
 print(f'Dist : {sumDist}')
